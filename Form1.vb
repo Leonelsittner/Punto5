8bbfd9f7529dab0451612d524489f4220e40de4b
leonel Sittner <leonelsittnerp20@gmail.com>
2023-06-24 12:12:49 -0300

    El comienzo

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim vector(10), Vectors(15) A, S, I As Integer  
+           For I = 1 To 10
+            vector(I) = Rnd() * 100
+        Next
 
     End Sub
 End Class
