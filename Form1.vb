7bcfb06dffa780794d4a440fb2f9cc6ba755cb2a
leonel Sittner <leonelsittnerp20@gmail.com>
2023-06-24 12:18:43 -0300

    Mitads

--- Form1.vb
+++ Form1.vb
@@ -1,8 +1,16 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim vector(10), Vectors(15) A, S, I As Integer  
-           For I = 1 To 10
+        Dim vector(10), Vectors(15), A, S, I As Integer
+        For I = 1 To 10
             vector(I) = Rnd() * 100
+        Next
+        For s = 1 To 15
+            For a = 1 To 2
+                I = I + 1
+                Vectors(S) = vector(I)
+                S = S + 1
+            Next
+            Vectors(S) = vector(I) + vector(I - 1)
         Next
 
     End Sub
